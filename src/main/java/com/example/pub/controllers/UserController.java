e915f335a7fec401ce27d9d9de2710caae175b97
Tomas Prajzler <tomas.prajzler@email.cz>
2023-10-19 18:51:52 +0200

    create getUserById endpoint

--- src/main/java/com/example/pub/controllers/UserController.java
+++ src/main/java/com/example/pub/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.example.pub.controllers;
 
+import com.example.pub.models.User;
 import com.example.pub.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -19,5 +23,14 @@ public class UserController {
     @GetMapping("")
     public ResponseEntity<?> getAllUsers(){
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id){
+        Optional<User> optionalUser = userService.getUserById(id);
+        if (optionalUser.isPresent()) {
+            return ResponseEntity.ok().body(optionalUser);
+        } else {
+            return ResponseEntity.status(404).body("User with this id doesn't exist");
+        }
     }
 }
--- src/main/java/com/example/pub/services/UserService.java
+++ src/main/java/com/example/pub/services/UserService.java
@@ -3,7 +3,10 @@ package com.example.pub.services;
 import com.example.pub.models.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     List<User> getAllUsers();
+
+    Optional<User> getUserById(Long id);
 }
