3a7f0a112f42a931ee68224f6bf5c566129c7e55
Tomas Prajzler <tomas.prajzler@email.cz>
2023-10-19 17:01:36 +0200

    create fields for drinks and orders

--- src/main/java/com/example/pub/models/Drink.java
+++ src/main/java/com/example/pub/models/Drink.java
@@ -1,4 +1,56 @@
 package com.example.pub.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "drinks")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Drink {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String productName;
+    private int price;
+    private boolean isForAdult;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if(price > 0){
+            this.price = price;
+        }
+    }
+
+    public boolean isForAdult() {
+        return isForAdult;
+    }
+
+    public void setForAdult(boolean forAdult) {
+        isForAdult = forAdult;
+    }
 }
--- src/main/java/com/example/pub/models/Order.java
+++ src/main/java/com/example/pub/models/Order.java
@@ -1,4 +1,55 @@
 package com.example.pub.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Table (name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String productName;
+    private int amount;
+    private int price;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        if (amount > 0) {
+            this.amount = amount;
+        }
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if (price > 0) {
+            this.price = price;
+        }
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
