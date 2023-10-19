a717aaf3b32f87ee511ad10481cd45f23fd1e092
Tomas Prajzler <tomas.prajzler@email.cz>
2023-10-19 17:28:01 +0200

    add user fields, role enum

---
+++ src/main/java/com/example/pub/models/Role.java
@@ -0,0 +1,5 @@
+package com.example.pub.models;
+
+public enum Role {
+    USER, ADMIN, BARTENDER
+}
--- src/main/java/com/example/pub/models/User.java
+++ src/main/java/com/example/pub/models/User.java
@@ -1,4 +1,134 @@
 package com.example.pub.models;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private boolean isActive;
+    private boolean isAdult;
+    private int pocket;
+    private LocalDate dateOfBirth;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    List<Order> orders;
+
+    public User(String name, LocalDate dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.isAdult = calculateIsAdult(dateOfBirth);
+        isActive = true;
+        pocket = 0;
+    }
+    private boolean calculateIsAdult(LocalDate dateOfBirth) {
+        LocalDate now = LocalDate.now();
+        Period age = Period.between(dateOfBirth, now);
+        return age.getYears() >= 18;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public void setAdult(boolean adult) {
+        isAdult = adult;
+    }
+
+    public int getPocket() {
+        return pocket;
+    }
+
+    public void setPocket(int pocket) {
+        if (pocket > 0) {
+            this.pocket = pocket;
+        }
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
+
+
+
+
